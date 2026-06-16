d4f77e4f6a4d3765a133ab274a9fa688b06a09b2
Ariel Soares <soaresariel017@gmail.com>
2026-06-16 13:35:01 -0300

    feat: configure BullMQ and Redis connection feat: create mail-queue module with producer and processor

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { MailModule } from 'src/mail/mail.module';
+import { MailQueueModule } from 'src/mail-queue/mail-queue.module';
 
 @Module({
-  imports: [DatabaseModule, MailModule],
+  imports: [DatabaseModule, MailModule, MailQueueModule],
   controllers: [UserController],
   providers: [UserService],
 })
